b98cc74d5dcb83277c576352e96f18fd9ddd2150
Rohun Banerji <rohunb@gmail.com>
2014-04-03 17:13:27 -0400

    started camera restrictions

--- Assets/Scripts/General/CameraMover.cs
+++ Assets/Scripts/General/CameraMover.cs
@@ -9,6 +9,8 @@ public class CameraMover : MonoBehaviour {
     public float minFov = 20.0f;
     public float maxFov = 85.0f;
 
+    public int terrainLayer = 8;
+
 	// Use this for initialization
 	void Start () {
 	    
@@ -16,11 +18,59 @@ public class CameraMover : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+       
+        
 	}
     public void Pan(Vector2 panVector)
     {
-        transform.Translate(new Vector3(panVector.x * camerMoveSpeed * Time.deltaTime, panVector.y * camerMoveSpeed * Time.deltaTime,0.0f));
+        bool canPan=false;
+        RaycastHit hit;
+        
+        Ray topLeft = Camera.main.ScreenPointToRay(new Vector3(0, 0, 0));
+        Ray topRight = Camera.main.ScreenPointToRay(new Vector3(Screen.width, 0, 0));
+        Ray botRight = Camera.main.ScreenPointToRay(new Vector3(Screen.width, Screen.height, 0));
+        Ray botLeft = Camera.main.ScreenPointToRay(new Vector3(0, Screen.height, 0));
+
+        Vector3 cameraOldPos = transform.position;
+        transform.Translate(new Vector3(panVector.x * camerMoveSpeed * Time.deltaTime, panVector.y * camerMoveSpeed * Time.deltaTime, 0.0f));
+
+        bool topRightOnTerrain = Physics.Raycast(topRight, out hit, Mathf.Infinity, 1 << terrainLayer);
+        bool topLeftOnTerrain = Physics.Raycast(topLeft, out hit, Mathf.Infinity, 1 << terrainLayer);
+        bool botRightOnTerrain = Physics.Raycast(botRight, out hit, Mathf.Infinity, 1 << terrainLayer);
+        bool botLeftOnTerrain = Physics.Raycast(botLeft, out hit, Mathf.Infinity, 1 << terrainLayer);
+
+        if(topRightOnTerrain && topLeftOnTerrain && botLeftOnTerrain && botRightOnTerrain)
+        {
+            canPan = true;
+        }
+
+        //if (panVector.x > 0f)
+        //{
+        //    if (topRightOnTerrain && botRightOnTerrain)
+        //        canPan = true;
+        //    else if (topRightOnTerrain && !botRightOnTerrain)
+        //    {
+
+        //    }
+        //}
+        //if(panVector.x<0f)
+        //{
+
+        //}
+        //if (panVector.y < 0f)
+        //{
+            
+        //}
+        //if (panVector.y > 0f)
+        //{
+            
+        //}
+        if (!canPan)
+        {
+
+           // transform.position = cameraOldPos;
+            transform.Translate(new Vector3(-panVector.x * camerMoveSpeed * Time.deltaTime, -panVector.y * camerMoveSpeed * Time.deltaTime, 0.0f));
+        }
     }
     public void Zoom(float zoomAmount)
     {
